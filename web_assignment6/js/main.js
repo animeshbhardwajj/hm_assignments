5de735abe55f3c307a59c9ab8a1a5d977799ee3b
Animesh Bhardwaj <animesh0422@gmail.com>
2022-11-06 20:06:43 +0530

    static page with little responseiveness

---
+++ web_assignment6/js/main.js
@@ -0,0 +1,7 @@
+function home() {
+    alert("You Are already in Home Page!");
+}
+
+function about() {
+    alert("This is the webpage where we have discussed definations of HTML, CSS, & JavaScript!");
+}
