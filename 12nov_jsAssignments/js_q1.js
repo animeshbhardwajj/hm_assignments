0eac2e705ad9f63d11be945a8a96cd1d3180cdea
Animesh Bhardwaj <animesh0422@gmail.com>
2022-11-13 16:02:00 +0530

    renamed some folders

---
+++ 12nov_jsAssignments/js_q1.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 3;
+var c = a + b;
+var d = a - b;
+console.log("The sum of "+a+" and "+b+" is: "+c);
+console.log("Diffrence b/w "+a+" and "+b+" is: "+d)
+
---
+++ jQuery_day1/js/main.js
@@ -0,0 +1,6 @@
+$ ( document) . ready (function( ) {
+    // Tag Selector
+    $( "button") . on(" click" (
+    css( "backgroundC010r" " navy" ) ;
+    " "white");
+    1
